4eaa9678266b01db68d736759aa8c1d0c8909570
tylerhkmontana <tylerhkmontana@gmail.com>
2022-10-03 17:10:23 -0400

    add pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+});
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,13 @@ import { AuthProvider } from '../context/authContext'
 
 function MyApp({ Component, pageProps }) {
   return <>
+    <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/logo.png"></link>
+        <meta name="theme-color" content="#fff" />
+    </Head>
   <AuthProvider>
-   <Component {...pageProps} />
+    <Component {...pageProps} />
   </AuthProvider>
   </>
 }
