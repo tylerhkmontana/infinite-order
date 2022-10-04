30b6ed279101f211435a139a57c0c126e92d77d7
tylerhkmontana <tylerhkmontana@gmail.com>
2022-10-04 14:03:13 -0400

    offline page added

---
+++ pages/_offline.js
@@ -0,0 +1,8 @@
+export default function Offline() {
+    return(
+        <div>
+            <h1>Welcome to Infinite Order</h1>
+            <p>You are currently offline. Please connect to internet.</p>
+        </div>
+    )
+}
