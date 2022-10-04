6ec99dd53850fb1d04df14dbcad22eeff879e0b6
tylerhkmontana <tylerhkmontana@gmail.com>
2022-10-04 13:56:00 -0400

    sw regiestered

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,24 @@
 import '../styles/globals.scss'
 import Head from 'next/head'
+import { useEffect } from 'react'
 import { AuthProvider } from '../context/authContext'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+       navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log("Service Worker registration successful with scope: ", registration.scope);
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, [])
+
   return <>
     <Head>
         <link rel="manifest" href="/manifest.json" />
